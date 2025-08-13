00eab1eca67b88949346d05478bb6910807609ac
aldren <aldrenarcenal.aa@gmail.com>
2025-08-13 21:28:52 +0800

    added tmdb config for api fetching

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,26 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import SearchBar from "@/components/searchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Image, ScrollView, Text, View } from "react-native";
+import { useRouter } from "expo-router";
 
 export default function Index() {
+  const router = useRouter();
   return (
-    <View className="items-center justify-center flex-1 ">
-      <Text className="text-5xl font-bold text-primary">HIIII</Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0"/>
+
+      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false}>
+        <Image source={icons.logo} className="w-20 h-10 mt-20 mb-5 mx-auto"/>
+
+        <View className="flex-1 mt-5">
+          <SearchBar onPress={() => {
+            router.push('/search');
+          }}
+          placeholder="Search for a movie"
+          />
+        </View>
+      </ScrollView>
     </View>
-    
   );
 }  
